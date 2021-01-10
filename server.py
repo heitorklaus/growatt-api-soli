b8e224089bed8f8229278443959f21153196e1e7
Heitor Klaus <heitorklaus@hotmail.com>
2021-01-10 19:07:50 -0300

    all

--- server.py
+++ server.py
@@ -1,6 +1,29 @@
 import os
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
 
+import sys
+import datetime
+from growatt import hash_password, GrowattApi, Timespan
+
+
+
+
+username = "solienergiasolar"
+password = "soli2020"
+
+
+
+with GrowattApi() as api:
+    api.login(username, password)
+    plant_info = api.plant_list()
+    #print(plant_info)
+
+    plant_id = plant_info["data"][0]["plantId"]
+    plant_detail = api.plant_detail(plant_id, Timespan.day, datetime.date.today())
+    #print(plant_detail)
 
 app = Flask(__name__)
 
@@ -8,7 +31,7 @@ app = Flask(__name__)
 def server():
     if request.headers.get('Authorization') == '42':
         return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "Não entre em pânico!"})
+    return jsonify(plant_detail)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
